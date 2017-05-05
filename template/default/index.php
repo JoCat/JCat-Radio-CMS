835d1c7ec38568a9282e6d8381b81ba301463bd2
Andrew Molchanov <mr.catmanrus@gmail.com>
2017-05-05 21:43:41 +0300

    dev_170505_update

--- template/default/index.php
+++ template/default/index.php
@@ -1,4 +1,4 @@
-<div style="text-align:center; font-size:2em; padding:1em;">
-Тестовая начальная страница<br>
-Вы можете изменить контент этой страницы в файле 'index.tpl'
+<div style="text-align:center; font-size:1.8em; padding:1em;">
+Начальная страница<br>
+Вы можете изменить контент этой страницы в файле <code>index.php</code>
 </div>
