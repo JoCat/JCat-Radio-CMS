c9d069b06c1a722cdcdd99a989e785d75a04a8bf
Andrew Molchanov <mr.catmanrus@gmail.com>
2017-06-20 02:51:03 +0300

    dev170620

--- engine/frontend/reg.php
+++ engine/frontend/reg.php
@@ -76,25 +76,53 @@ if (!empty($_POST))
                     $url = 'http://'. $_SERVER['HTTP_HOST'] .'/reg/activate/'. $key;
                     $message = <<<MSG
 <html>
-  <head>
-    <title>Регистрация на сайте {$_SERVER['HTTP_HOST']} </title>
-  </head>
-  <body>
-    <p>Для активации Вашего аккаунта пройдите по ссылке: </p>
-    <a href="$url">Активация аккаунта</a>
-  </body>
+<head>
+<meta charset="utf-8">
+<title>Регистрация на сайте {$_SERVER['HTTP_HOST']}</title>
+<style>
+body {
+    margin: 0;
+    font-family: sans-serif;
+}
+h1 {
+    margin: 0;
+    padding: 0.5em;
+    font-size: 1.5em;
+    color: #fff;
+    text-align: center;
+    background-color: #00bbff;
+}
+h2 {
+    font-size: 1.2em;
+}
+.wrap {
+    margin: 10px 5px;
+}
+</style>
+</head>
+<body>
+<h1>Регистрация на сайте {$_SERVER['HTTP_HOST']}</h1>
+<div class="wrap">
+<h2>Для активации Вашего аккаунта пройдите по ссылке: </h2>
+<a href="$url">Активация аккаунта</a>
+<p>Вы получили это письмо, так как этот e-mail адрес был использован при регистрации на сайте. Если Вы не регистрировались на этом сайте, просто проигнорируйте это письмо и удалите его.<br>
+Данное сообщение создано автоматически, отвечать на него не нужно.</p>
+</div>
+</body>
 </html>
 MSG;
                     
                     //Формируем заголовки для почтового сервера
                     $headers = "MIME-Version: 1.0\r\n";
                     $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
-                    $headers .= "To: ". $_POST['email'] . "\r\n";
                     $headers .= "From: ". $config->admin_mail ."\r\n";
-                    $headers .= "Date: ". date('D, d M Y h:i:s O');
 
                     //Отправляем данные на почту
-                    mail($_POST['email'], 'Регистрация на сайте ' . $_SERVER['HTTP_HOST'], $message, $headers);
+                    mail($_POST['email'],
+                        '=?utf-8?B?' . base64_encode('Регистрация на сайте ' . $_SERVER['HTTP_HOST']) . '?=',
+                        $message,
+                        $headers
+                    );
                         
                     $helpers->addMessage('Вы успешно зарегистрировались! Пожалуйста активируйте свой аккаунт!');
                 }
